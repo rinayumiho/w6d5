cabce79ac595a8476e57ba94860406f0d4ddc745
dustin <dustin.adler@yahoo.com>
2021-07-23 16:56:41 -0500

    almost finished edit

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -8,4 +8,25 @@ class CatsController < ApplicationController
         @cat = Cat.find_by(id: params[:id])
         render :show
     end
+
+    def new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :birthday, :color, :sex, :description)
+    end
+
 end
